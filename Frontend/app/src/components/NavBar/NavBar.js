df0c08efa06076498363637386d41ced6e9d4abf
Saar Salhov <86562519+saarsalhov@users.noreply.github.com>
2022-06-09 19:36:32 +0300

    fix problem in navbar.js file

--- Frontend/app/src/components/NavBar/NavBar.js
+++ Frontend/app/src/components/NavBar/NavBar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MenuList from './MenuList';
 import Home from '../pages/Home'
-import About from '../pages/About'
 import Receipts from '../pages/Receipts'
 import SignUp from '../pages/SignUp'
 import LogIn from '../pages/LogIn'
